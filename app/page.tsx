3753e157436c0792dc4d7c34255b5c3dba230592
Eric Murrell <82235988+EMurrell@users.noreply.github.com>
2024-07-29 16:36:25 -0400

    update h2

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,12 @@ import { fadeInCode } from "./code-strings/fadeIn";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-2 sm:p-6 lg:p-24">
-      <h1 className="text-3xl sm:text-4xl lg:text-5xl">Hello World</h1>
-      <h2 className="text-xl sm:text-2xl lg:text-3xl mt-4">
-        Description with delay
-      </h2>
+      <h1 className="text-3xl sm:text-4xl lg:text-5xl">Animation Mania</h1>
+      <ul className="text-sm sm:text-base lg:text-lg my-4">
+        <li>Install Framer-motion</li>
+        <li>Copy/paste animation into their own file</li>
+        <li>Use in your code as `div`</li>
+      </ul>
       <CodePreview
         animationComponent={FadeIn}
         code={fadeInCode}
