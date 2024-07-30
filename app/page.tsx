00a04132b96e510961b61ca6da0903ec3d1434ae
Eric Murrell <82235988+EMurrell@users.noreply.github.com>
2024-07-30 09:24:20 -0400

    update max width main

--- app/page.tsx
+++ app/page.tsx
@@ -7,22 +7,24 @@ import { fadeInCode } from "./code-strings/fadeIn";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-2 sm:p-6 lg:p-24">
-      <h1 className="text-3xl sm:text-4xl lg:text-5xl">Motion Repo</h1>
-      {/* <ul className="text-sm sm:text-base lg:text-lg my-4">
+      <div className="mx-auto max-w-5xl w-full">
+        <h1 className="text-3xl sm:text-4xl lg:text-5xl">Motion Repo</h1>
+        {/* <ul className="text-sm sm:text-base lg:text-lg my-4">
         <li>Install Framer-motion</li>
         <li>Copy/paste animation into their own file</li>
         <li>Use in your code as `div`</li>
       </ul> */}
-      <CodePreview
-        animationComponent={FadeIn}
-        code={fadeInCode}
-        animationProps={{
-          delay: 0.4,
-          up: true,
-          className: "text-xl sm:text-2xl lg:text-3xl mt-4",
-        }}
-        text="Fade In"
-      />
+        <CodePreview
+          animationComponent={FadeIn}
+          code={fadeInCode}
+          animationProps={{
+            delay: 0.4,
+            up: true,
+            className: "text-xl sm:text-2xl lg:text-3xl mt-4",
+          }}
+          text="Fade In"
+        />
+      </div>
     </main>
   );
 }
