77e184f2d0e503820259d45754fda157a4708d48
Eric Murrell <82235988+EMurrell@users.noreply.github.com>
2024-08-01 09:37:13 -0400

    add headerand logo

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,3 +1,15 @@
+import Image from "next/image";
+
 export default function Header() {
-  return <header className="w-full max-w-5xl flex"></header>;
+  return (
+    <header className="w-full max-w-5xl flex">
+      <Image
+        src="/logo.png"
+        alt="Trailbridge Web Logo"
+        className=""
+        height={100}
+        width={100}
+      />
+    </header>
+  );
 }
