aabe86d62c0a9efc2da7b6235f8d6b104cc40375
Eric Murrell <82235988+EMurrell@users.noreply.github.com>
2024-08-01 10:44:13 -0400

    update hero positioning

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -3,37 +3,36 @@ import TwitterShare from "./TwitterShare";
 export default function Hero() {
   return (
     <section>
-      <div className="w-full flex justify-between">
-        <h1 className="text-5xl lg:text-6xl font-medium">Motion Repo</h1>
-      </div>
-      <div className=" flex flex-col lg:flex-row justify-between w-full">
-        <div className="max-w-[90%] lg:w-[70%] text-sm mt-2 font-light space-y-2 lg:pr-28">
-          <h2 className="text-[1.125rem] leading-[1.5rem] lg:text-lg text-balance mb-4 font-medium">
-            A collection of animations built with Framer Motion.
+      <div className="w-full flex flex-col lg:flex-row justify-between">
+        <div className="flex flex-col lg:w-[70%] mt-auto pr-12 lg:pr-36">
+          <h1 className="text-6xl lg:text-7xl font-medium">Motion Repo</h1>
+
+          <h2 className="text-[1.125rem] leading-[1.5rem] lg:text-[18px] mt-4">
+            A collection of accessible, customizable animations built with
+            Framer Motion.
           </h2>
-          <p>
-            {" "}
-            To get started, ensure you have{" "}
-            <a
-              href="https://www.framer.com/motion/introduction/"
-              className="text-blue-600 hover:text-blue-400 transition ease-in-out duration-200">
-              Framer-Motion
-            </a>{" "}
-            and{" "}
-            <a
-              href="https://tailwindcss.com/docs/installation"
-              className="text-blue-600 hover:text-blue-400 transition ease-in-out duration-200">
-              Tailwind CSS{" "}
-            </a>{" "}
-            installed and configured in your project.{" "}
-          </p>
-          <p>
-            Copy the provided code into your files. Import and use the animation
-            components like regular elements (e.g., div, span).
-          </p>
         </div>
 
-        <ul className="text-xs sm:text-sm font-light my-4 lg:w-[30%] max-w-[90%] lg:p-4 space-y-2 lg:border rounded-2xl">
+        <p className="lg:w-[30%] text-xs sm:text-sm font-light mb-1 pr-12 lg:pr-0 mt-4 lg:mt-0">
+          {" "}
+          To get started, ensure you have{" "}
+          <a
+            href="https://www.framer.com/motion/introduction/"
+            className="text-blue-600 hover:text-blue-400 transition ease-in-out duration-200">
+            Framer-Motion
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://tailwindcss.com/docs/installation"
+            className="text-blue-600 hover:text-blue-400 transition ease-in-out duration-200">
+            Tailwind CSS{" "}
+          </a>{" "}
+          installed and configured in your project. Copy the provided code into
+          your files. Import and use the animation components like regular
+          elements (e.g., div, span).
+        </p>
+
+        {/* <ul className="text-xs sm:text-sm font-light my-4 lg:w-[30%] max-w-[90%] lg:p-4 space-y-2 lg:border rounded-2xl">
           <li>
             <span className="font-medium">Customizable: </span>Adjust parameters
             like duration and delay via props.
@@ -46,7 +45,7 @@ export default function Hero() {
             <span className="font-medium">Flexible: </span>Style and customize
             as needed.
           </li>
-        </ul>
+        </ul> */}
       </div>
     </section>
   );
