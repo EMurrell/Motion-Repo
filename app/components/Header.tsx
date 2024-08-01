d15abb88ec93c8e89b26b3e05873e965dbe3695f
Eric Murrell <82235988+EMurrell@users.noreply.github.com>
2024-08-01 09:45:41 -0400

    add x padding: header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,7 +3,7 @@ import TwitterShare from "./TwitterShare";
 
 export default function Header() {
   return (
-    <header className="w-full flex py-6">
+    <header className="w-full flex py-6 px-2 sm:px-6 lg:px-24">
       <div className="max-w-5xl flex mx-auto w-full justify-between">
         <Image
           src="/logo.png"
