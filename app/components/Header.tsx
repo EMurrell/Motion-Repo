2a7ded45fb65d8a5f3611188d51b3b87ac2564f3
Eric Murrell <82235988+EMurrell@users.noreply.github.com>
2024-08-01 09:40:07 -0400

    update header styles and button

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,15 +1,19 @@
 import Image from "next/image";
+import TwitterShare from "./TwitterShare";
 
 export default function Header() {
   return (
-    <header className="w-full max-w-5xl flex">
-      <Image
-        src="/logo.png"
-        alt="Trailbridge Web Logo"
-        className=""
-        height={100}
-        width={100}
-      />
+    <header className="w-full flex py-6">
+      <div className="max-w-5xl flex mx-auto w-full justify-between">
+        <Image
+          src="/logo.png"
+          alt="Trailbridge Web Logo"
+          className=""
+          height={50}
+          width={50}
+        />
+        <TwitterShare />
+      </div>
     </header>
   );
 }
