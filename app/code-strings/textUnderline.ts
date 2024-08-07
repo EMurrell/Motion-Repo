75434295a011e79a849e9ed05e4410df18f646cb
Eric Murrell <82235988+EMurrell@users.noreply.github.com>
2024-08-07 10:30:20 -0400

    update underline

--- app/code-strings/textUnderline.ts
+++ app/code-strings/textUnderline.ts
@@ -11,7 +11,7 @@ export default function TextUnderline({
   className = "",
 }: TextUnderlineProps) {
   return (
-    <span className="group inline-block cursor-pointer">
+    <span className={\`group inline-block cursor-pointer \${className}\`}>
       {children}
       <span className="block h-0.5 lg:h-1 w-full bg-black transition-all duration-500 motion-reduce:duration-0 ease-in-out max-w-0 group-hover:max-w-full" />
     </span>
