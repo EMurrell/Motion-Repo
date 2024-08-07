28871b05d5bab2a1d6a2d433917eddf73b400a4f
Eric Murrell <82235988+EMurrell@users.noreply.github.com>
2024-08-07 12:51:19 -0400

    prevent layout shift

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,13 +4,14 @@ import GithubLink from "./GithubLink";
 
 export default function Header() {
   return (
-    <header className="w-full flex py-6 px-2 sm:px-6 lg:px-24">
+    <header className="w-full flex py-6 px-2 sm:px-6 lg:px-24 h-[95px]">
       <div className="max-w-5xl flex mx-auto w-full justify-between">
         <Image
           src="/logo.png"
           alt="Trailbridge Web Logo"
           height={40}
           width={40}
+          priority={true}
         />
         <div className="flex gap-2  items-center">
           <GithubLink />
